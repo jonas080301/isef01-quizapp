d40c7076051fa688fc1484cd5d105c3f55e7c15b
Jonas Karl <jonas.karl@iu-study.org>
2023-10-12 18:59:00 +0200

    Added Styling for Toast

--- plugins/toast.ts
+++ plugins/toast.ts
@@ -1,6 +1,6 @@
 import { defineNuxtPlugin } from '#app'
 import * as vt from 'vue-toastification'
-
+import "vue-toastification/dist/index.css";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(vt.default)
   return {
