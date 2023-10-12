b0f7c2df76a0dab2587a687fa93e880bec4bb094
Jonas Karl <jonas.karl@iu-study.org>
2023-10-12 18:54:53 +0200

    Bugfix for toast

--- plugins/toast.ts
+++ plugins/toast.ts
@@ -1,6 +1,5 @@
 import { defineNuxtPlugin } from '#app'
 import * as vt from 'vue-toastification'
-import '@/assets/css/toast.scss'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(vt.default)
